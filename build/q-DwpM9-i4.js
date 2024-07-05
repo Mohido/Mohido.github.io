eaa76d2bd5218bf8d2981643cde293b3f296bb46
Mohammed Al-Mahdawi <m.ghaithalmahdawi@gmail.com>
2024-07-05 21:30:30 +0200

    Fixed Origin

--- build/q-DwpM9-i4.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./q-uemlvruI.js";import{D as r,q as t}from"./q-CUnOVDMa.js";const a=r(t(()=>o(()=>import("./q-DIpXMnFk.js"),[]),"s_3sccYCDd1Z0"));export{a as default};
---
+++ build/q-assPpAq_.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./q-uemlvruI.js";import{D as r,q as t}from"./q-CUnOVDMa.js";const a=r(t(()=>o(()=>import("./q-vnz4MAlv.js"),[]),"s_3sccYCDd1Z0"));export{a as default};
